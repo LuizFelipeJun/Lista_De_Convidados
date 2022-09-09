36953cf882dc629dcf7ff96c69eb698e285dd1d9
LuizFelipeJun <104800410+LuizFelipeJun@users.noreply.github.com>
2022-09-09 20:08:00 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
 var nomes=[];
 function enviar() {
-   var displaynome=[];
-   for(var indice=1; indice<=4; indice++) {
-      var nome=document.getElementById("convidado1"+indice).value;
+   var nome=document.getElementById("convidado1").value;
+   nomes.push(nome);
+   document.getElementById("NomeConvidados").innerHTML=nomes.toString();
+}
+
+function mostrar() {
+   var listaNova=nomes.join("<br>");
+   document.getElementById("NomeConvidadoColuna").innerHTML=listaNova.toString();
+   document.getElementById("buttonOrganizar").style.display="block";
+}
+
+function organiza() {
+   nomes.sort();
+   var listaOrganizada=nomes.join("<br>");
+   document.getElementById("listaOrganizada").innerHTML=listaOrganizada.toString();
+}
+
+function pesquisarNome() {
+   var nomePesquisado=document.getElementById("pesquisa").value;
+   var indice;
+   var ContadorDeNome=0;
+   for(indice=0; indice<nomes.length; indice++) {
+      if(nomePesquisado==nomes[indice]) {
+         ContadorDeNome++;
+      }
    }
 
-   var comprimentoListaNome=nomes.length;
-   for(var indice=0; indice<comprimentoListaNome; indice++) {
-      displaynome.push("<span> "+nomes[indice]+"</span>");
-   }
+   document.getElementById("nomeConvidadoPesquisado").innerHTML="Nome " + nomePesquisado + " foi encontrado " + ContadorDeNome + " vezes";
+}
